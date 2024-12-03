690494d368640e67a0782d41ca0e92ea234d8989
Ira <ipp@tpu.ru>
2024-12-03 11:39:21 +0700

    remove

--- Project/Project.cpp
+++ Project/Project.cpp
@@ -6,11 +6,7 @@
 int main()
 {
     //vika: Изменение 1
-<<<<<<< HEAD
-    std::cout << "hello world!\n";
-=======
     std::cout << "Hello!\n";
->>>>>>> 89856369e4db1f3ff18cc29c28a6632341b1722
     std::cout << "Перевод: Привет Мир!\n";
 }
 
